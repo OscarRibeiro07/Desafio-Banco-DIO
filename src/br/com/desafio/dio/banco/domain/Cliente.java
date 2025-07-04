ba07ca0b370497f3163728a93b57da6a96cecf1f
OscarRibeiro07 <negociante2t@gmail.com>
2025-07-04 15:32:46 -0300

    implementa classe de cliente

--- src/br/com/desafio/dio/banco/domain/Cliente.java
+++ src/br/com/desafio/dio/banco/domain/Cliente.java
@@ -3,5 +3,21 @@ package br.com.desafio.dio.banco.domain;
 public class Cliente {
 
    private String nome;
+   private int idade;
+   private String profissao;
 
+   public Cliente(String nome, int idade, String profissao) {
+      this.nome = nome;
+      this.idade = idade;
+      this.profissao = profissao;
+   }
+
+   @Override
+   public String toString() {
+      return "Cliente{" +
+              "nome='" + nome + '\'' +
+              ", idade=" + idade +
+              ", profissao='" + profissao + '\'' +
+              '}';
+   }
 }
