0f2d21319f645745b66dfec957a2a1cbdbe6d9a7
OscarRibeiro07 <negociante2t@gmail.com>
2025-07-05 09:49:28 -0300

    teste de insercao de instancia de cliente e conta

--- src/br/com/desafio/dio/banco/TestaContaMain.java
+++ src/br/com/desafio/dio/banco/TestaContaMain.java
@@ -8,14 +8,24 @@ import br.com.desafio.dio.banco.domain.ContaPoupanca;
 public class TestaContaMain {
     public static void main(String[] args) {
         Cliente rafael = new Cliente("Rafael", 25, "Soldador");
+        Cliente manuel = new Cliente("manuel", 25, "Cozinheiro");
+        Cliente jessica = new Cliente("Jessica", 25, "Administradora");
 
-        Conta  cc = new ContaCorrente(rafael);
-        Conta cp = new ContaPoupanca(rafael);
+        Conta contaRafael = new ContaCorrente(rafael);
+        Conta contaManuel = new ContaPoupanca(manuel);
+        Conta contaJessica = new ContaPoupanca(jessica);
+        contaManuel.depositar(150);
+        contaRafael.depositar(150);
+        contaJessica.depositar(150);
 //        cc.setCliente(rafael);
         System.out.println("==== EXTRATO DA CONTA CORRENTE  ====\n" );
-        System.out.println(cc);
+        System.out.println(contaRafael);
         System.out.println("==== EXTRATO DA CONTA POUPANCA ====\n");
-        System.out.println(cc);
+        System.out.println(contaJessica);
+        System.out.println("==== EXTRATO DA CONTA POUPANCA ====\n");
+        System.out.println(contaManuel);
+
+
 
 
 
